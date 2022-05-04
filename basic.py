e18835f912f2caa4d7c61bcfd3c36f00042263b1
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2022-05-04 10:32:00 +0700

    :art: Format the code for module support, add some basic built-in module, and fix some bugs

--- basic.py
+++ basic.py
@@ -5,6 +5,8 @@
 from string_with_arrows import *
 
 import string
+import os
+import math
 
 #######################################
 # CONSTANTS
@@ -1228,6 +1230,17 @@ class Number(Value):
     def __repr__(self):
         return str(self.value)
 
+###################################
+# CONSTANT NUMBER
+###################################
+
+Number.null = Number(0)
+Number.false = Number(0)
+Number.true = Number(1)
+Number.pi = Number(math.pi)
+
+###################################
+
 class String(Value):
     def __init__(self, value):
         super().__init__()
@@ -1253,6 +1266,9 @@ class String(Value):
         copy.set_pos(self.pos_start, self.pos_end)
         copy.set_context(self.context)
         return copy
+    
+    def __str__(self):
+        return self.value
     
     def __repr__(self):
         return f'"{self.value}"'
@@ -1304,46 +1320,73 @@ class List(Value):
             return None, Value.illegal_operation(self, other)
     
     def copy(self):
-        copy = List(self.elements[:])
+        copy = List(self.elements)
         copy.set_pos(self.pos_start, self.pos_end)
         copy.set_context(self.context)
         return copy
     
+    def __str__(self):
+        return ", ".join([str(x) for x in self.elements])
+
     def __repr__(self):
         return f'[{", ".join([str(x) for x in self.elements])}]'
 
-class Module(Value):
-    def __init__(self, name, body_node, arg_names):
+class BaseModule(Value):
+    def __init__(self, name):
         super().__init__()
         self.name = name or "<anonymous>"
+
+    def generate_new_context(self):
+        new_context = Context(self.name, self.context, self.pos_start)
+        new_context.symbol_table = SymbolTable(new_context.parent.symbol_table)
+        return new_context
+    
+    def check_args(self, arg_names, args):
+        res = RTResult()
+
+        if len(args) > len(arg_names):
+            return res.failure(RTError(
+                self.pos_start, self.pos_end,
+                f"{len(args) - len(arg_names)} too many args passed into '{self.name}'",
+				self.context
+            ))
+        
+        if len(args) < len(arg_names):
+            return res.failure(RTError(
+				self.pos_start, self.pos_end,
+				f"{len(arg_names) - len(args)} too few args passed into '{self.name}'",
+				self.context
+			))
+        
+        return res.success(None)
+    
+    def populate_args(self, arg_names, args, exec_ctx):
+        for i in range(len(args)):
+            arg_name = arg_names[i]
+            arg_value = args[i]
+            arg_value.set_context(exec_ctx)
+            exec_ctx.symbol_table.set(arg_name, arg_value)
+    
+    def check_and_populate_args(self, arg_names, args, exec_ctx):
+        res = RTResult()
+        res.register(self.check_args(arg_names, args))
+        if res.error: return res
+        self.populate_args(arg_names, args, exec_ctx)
+        return res.success(None)
+
+class Module(BaseModule):
+    def __init__(self, name, body_node, arg_names):
+        super().__init__(name)
         self.body_node = body_node
         self.arg_names = arg_names
     
     def execute(self, args):
         res = RTResult()
         interpreter = Interpreter()
-        new_context = Context(self.name, self.context, self.pos_start)
-        new_context.symbol_table = SymbolTable(new_context.parent.symbol_table)
+        exec_ctx = self.generate_new_context()
 
-        if len(args) > len(self.arg_names):
-            return res.failure(RTError(
-                self.pos_start, self.pos_end,
-                f"{len(args) - len(self.arg_names)} too many args passed into '{self.name}'",
-				self.context
-            ))
-        
-        if len(args) < len(self.arg_names):
-            return res.failure(RTError(
-				self.pos_start, self.pos_end,
-				f"{len(self.arg_names) - len(args)} too few args passed into '{self.name}'",
-				self.context
-			))
-        
-        for i in range(len(args)):
-            arg_name = self.arg_names[i]
-            arg_value = args[i]
-            arg_value.set_context(new_context)
-            new_context.symbol_table.set(arg_name, arg_value)
+        res.register(self.check_and_populate_args(self.arg_names, args, exec_ctx))
+        if res.error: return res
         
         value = res.register(interpreter.visit(self.body_node, new_context))
         if res.error: return res
@@ -1357,6 +1400,171 @@ class Module(Value):
     
     def __repr__(self):
         return f"<module {self.name}>"
+
+class BuiltInModule(BaseModule):
+    def __init__(self, name):
+        super().__init__(name)
+    
+    def execute(self, args):
+        res = RTResult()
+        exec_ctx = self.generate_new_context()
+
+        method_name = f'execute_{self.name}'
+        method = getattr(self, method_name, self.no_visit_method)
+
+        res.register(self.check_and_populate_args(method.arg_names, args, exec_ctx))
+        if res.error: return res
+
+        return_value = res.register(method(exec_ctx))
+        if res.error: return res
+        return res.success(return_value)
+    
+    def no_visit_method(self, node, context):
+        raise Exception(f'No execute_{self.name} method defined')
+    
+    def copy(self):
+        copy = BuiltInModule(self.name)
+        copy.set_context(self.context)
+        copy.set_pos(self.pos_start, self.pos_end)
+        return copy
+    
+    def __repr__(self):
+        return f"<built-in module {self.name}>"
+
+    ###################################
+    # Add built-in module here
+    # name      : execute_<module_name>
+    # example   : execute_print
+    ###################################
+
+    def execute_print(self, exec_ctx):
+        print(str(exec_ctx.symbol_table.get('value')))
+        return RTResult().success(Number.null)
+    execute_print.arg_names = ['value'] # Argument for built-in module. In this case, execute_print will only have 1 argument called 'value'
+
+    def execute_print_ret(self, exec_ctx):
+        return RTResult().success(String(str(exec_ctx.symbol_table.get('value'))))
+    execute_print_ret.arg_names = ['value']
+
+    def execute_input(self, exec_ctx):
+        text = input()
+        return RTResult().success(String(text))
+    execute_input.arg_names = []
+
+    def execute_input_int(self, exec_ctx):
+        while True:
+            text = input()
+            try:
+                number = int(text)
+                break
+            except ValueError:
+                print(f"'{text}' must be an integer. Try again!")
+        return RTResult().success(Number(number))
+    execute_input_int.arg_names = []        
+
+    def execute_clear(self, exec_ctx):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        return RTResult().success(Number.null)
+    execute_clear.arg_names = []
+
+    def execute_is_number(self, exec_ctx):
+        is_number = isinstance(exec_ctx.symbol_table.get("value"), Number)
+        return RTResult().success(Number.true if is_number else Number.false)
+    execute_is_number.arg_names = ['value']
+
+    def execute_is_string(self, exec_ctx):
+        is_string = isinstance(exec_ctx.symbol_table.get("value"), String)
+        return RTResult().success(Number.true if is_string else Number.false)
+    execute_is_string.arg_names = ['value']
+
+    def execute_is_list(self, exec_ctx):
+        is_list = isinstance(exec_ctx.symbol_table.get("value"), List)
+        return RTResult().success(Number.true if is_list else Number.false)
+    execute_is_list.arg_names = ['value']
+
+    def execute_is_module(self, exec_ctx):
+        is_module = isinstance(exec_ctx.symbol_table.get("value"), BaseModule)
+        return RTResult().success(Number.true if is_module else Number.false)
+    execute_is_module.arg_names = ['value']
+
+    def execute_append(self, exec_ctx):
+        list_ = exec_ctx.symbol_table.get('list')
+        value = exec_ctx.symbol_table.get('value')
+
+        if not isinstance(list_, List):
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                "First argument must be list",
+                exec_ctx
+            ))
+        
+        list_.elements.append(value)
+        return RTResult().success(Number.null)
+    execute_append.arg_names = ['list', 'value']
+
+    def execute_pop(self, exec_ctx):
+        list_ = exec_ctx.symbol_table.get('list')
+        index = exec_ctx.symbol_table.get('index')
+
+        if not isinstance(list_, List):
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                "First argument must be list",
+                exec_ctx
+            ))
+        
+        if not isinstance(index, Number):
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                "Second argument must be number",
+                exec_ctx
+            ))
+        
+        try:
+            element = list_.elements.pop(index.value)
+        except:
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                'Element at this index could not be removed from list because index is out of bounds',
+                exec_ctx
+            ))
+        return RTResult().success(element)
+    execute_pop.arg_names = ['list', 'index']
+
+    def execute_extend(self, exec_ctx):
+        listA = exec_ctx.symbol_table.get('listA')
+        listB = exec_ctx.symbol_table.get('listB')
+
+        if not isinstance(listA, List):
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                "First argument must be list",
+                exec_ctx
+            ))
+        
+        if not isinstance(listB, List):
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                "First argument must be list",
+                exec_ctx
+            ))
+        
+        listA.elements.extend(listB.elements)
+        return RTResult().success(Number.null)
+    execute_extend.arg_names = ["listA", "listB"]
+
+BuiltInModule.print     = BuiltInModule("print")
+BuiltInModule.print_ret = BuiltInModule("print_ret")
+BuiltInModule.input     = BuiltInModule("input")
+BuiltInModule.input_int = BuiltInModule("input_int")
+BuiltInModule.clear     = BuiltInModule("clear")
+BuiltInModule.is_number = BuiltInModule("is_number")
+BuiltInModule.is_string = BuiltInModule("is_string")
+BuiltInModule.is_list   = BuiltInModule("is_list")
+BuiltInModule.is_module = BuiltInModule("is_module")
+BuiltInModule.append    = BuiltInModule("append")
+BuiltInModule.pop       = BuiltInModule("pop")
+BuiltInModule.extend    = BuiltInModule("extend")
 
 #######################################
 # CONTEXT
@@ -1404,6 +1612,10 @@ class Interpreter:
         raise Exception(f'No visit_{type(node).__name__} method defined')
     
     ###################################
+    # Add interpreter for node here
+    # name      : visit_<node_name>
+    # example   : visit_NumberNode
+    ###################################
 
     def visit_NumberNode(self, node, context):
         return RTResult().success(
@@ -1439,7 +1651,7 @@ class Interpreter:
                 context
             ))
         
-        value = value.copy().set_pos(node.pos_start, node.pos_end)
+        value = value.copy().set_pos(node.pos_start, node.pos_end).set_context(context)
         return res.success(value)
     
     def visit_VarAssignNode(self, node, context):
@@ -1604,6 +1816,7 @@ class Interpreter:
 
         return_value = res.register(value_to_call.execute(args))
         if res.error: return res
+        return_value = return_value.copy().set_pos(node.pos_start, node.pos_end).set_context(context)
         return res.success(return_value)
 
 #######################################
@@ -1611,9 +1824,22 @@ class Interpreter:
 #######################################
 
 global_symbol_table = SymbolTable()
-global_symbol_table.set("NULL", Number(0))
-global_symbol_table.set("TRUE", Number(1))
-global_symbol_table.set("FALSE", Number(0))
+global_symbol_table.set("Null", Number.null)
+global_symbol_table.set("True", Number.false)
+global_symbol_table.set("False", Number.true)
+global_symbol_table.set("Pi", Number.pi)
+global_symbol_table.set("print", BuiltInModule.print)
+global_symbol_table.set("print_ret", BuiltInModule.print_ret)
+global_symbol_table.set("input", BuiltInModule.input)
+global_symbol_table.set("input_int", BuiltInModule.input_int)
+global_symbol_table.set("clear", BuiltInModule.clear)
+global_symbol_table.set("is_number", BuiltInModule.is_number)
+global_symbol_table.set("is_string", BuiltInModule.is_string)
+global_symbol_table.set("is_list", BuiltInModule.is_list)
+global_symbol_table.set("is_module", BuiltInModule.is_module)
+global_symbol_table.set("append", BuiltInModule.append)
+global_symbol_table.set("pop", BuiltInModule.pop)
+global_symbol_table.set("extend", BuiltInModule.extend)
 
 def run(fn, text):
     # Generate tokens
